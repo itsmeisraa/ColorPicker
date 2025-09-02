f782b6cbdc155975c6b8fe5045a679b6546eb1b3
israa chiheb <israaisraa202000@gmail.com>
2025-09-03 00:41:20 +0100

    now its ready

--- src/Try.jsx
+++ src/Try.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 
 function Try() {
+const [color, setcolor] = useState("#FFFF")
+function changecolor(event){
+  setcolor(event.target.value);
 
+}
+  return(<div className="card">
+    <h1>color picker</h1>
+    <div className="color" style={{backgroundColor:color}}>
+      <p>{color}</p>
+    </div>
+     <label className="here"> please select the color </label>
+     <input type="color" value={color} onChange={changecolor} />
 
-  return();
+  </div>);
 }
 
 export default Try
